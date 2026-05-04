3ce230829c8a787b3054fc269b998fa03e3f427b
Matt Caldwell <StoatPower@users.noreply.github.com>
2026-05-04 17:20:46 -0400

    removed dead code in takeoff_base.m

--- src/takeoff_base.m
+++ src/takeoff_base.m
@@ -16,56 +16,7 @@ function base = takeoff_base(polars_path)
     base.CL_max = aero.CL(base.cl_max, base.eta); % 3D CL max
     base.CD_max = aero.CD(base.cd_cl_max, base.CL_max, p.e, p.AR); % 3D CD max
     base.CL = base.CL_max; % 3D CL is at 3D CL_max for takeoff
-    base.CD = base.CD_max; % 3D CD is at 3D CD_max for takeoff   
-    
-    % 
-    % % Assumed initial weight
-    % W0 = aero.init_W0_perc(opts.m_fuel_perc, p.m_body, p.m_fuel_max, p.g);
-    % out.W0 = W0;
-    % 
-    % % takeoff velocity
-    % V_to = aero.takeoff_velocity(W0, p.rho_ORD, p.S, CL_max);
-    % out.V_to = V_to;
-    % 
-    % % total lift
-    % L = aero.L(V_to, CL, p.rho_ORD, p.S);
-    % out.L = L;
-    % 
-    % % total drag
-    % D = aero.D(V_to, CD, p.rho_ORD, p.S);
-    % out.D = D;
-    % 
-    % % take off thrust = max thrust
-    % T_to = p.T_max;
-    % out.T_to = T_to;
-    % 
-    % % takeoff distance with rolling friction
-    % x_to = aero.x_to(V_to, CL_max, CD_max, T_to, W0, p.rho_ORD, p.S, p.g, p.mu_r);
-    % out.x_to = x_to;
-    % 
-    % % takeoff distance without rolling friction
-    % % x_to_no_fric = aero.x_to(V_to, CL_max, CD_max, T_to, W0, p.rho_ORD, p.S, p.g, 0);
-    % % out.x_to_no_fric = x_to_no_fric;
-    % 
-    % % takeoff time with rolling friction
-    % t_to = aero.t_takeoff(x_to, V_to);
-    % out.t_to = t_to;
-    % 
-    % % takeoff time without rolling friction
-    % % t_to_no_fric = aero.t_takeoff(x_to_no_fric, V_to);
-    % % out.t_to_no_fric = t_to_no_fric;
-    % 
-    % % mass flow rate 
-    % mf_rate_to = aero.mass_flow_rate(T_to, p.SFC);
-    % out.mf_rate_to = mf_rate_to;
-    % 
-    % % mass flow total
-    % mf_total_to = aero.mass_flow_total(mf_rate_to, t_to);
-    % out.mf_total_to = mf_total_to;
-    % 
-    % % final weight at take off
-    % W_to = W0 - mf_total_to*p.g;
-    % out.W_to = W_to;
+    base.CD = base.CD_max; % 3D CD is at 3D CD_max for takeoff
 end
 
 
